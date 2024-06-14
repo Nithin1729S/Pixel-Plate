29197ae899658c57cf7b69da83d2ba1d0a5e29f2
nithin <sureshnithin1729@gmail.com>
2024-06-14 22:51:45 +0530

    mern cluster connected and routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
-import express, {Request,Response} from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import "dotenv/config"
 
-const app=express()
+import mongoose from "mongoose"
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+    console.log("MONGODB Connected")
+})
+
+const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/test',async(req:Request,res:Response)=>{
-    res.json({message:'Hello'});
+app.get('/test', async (req: Request, res: Response) => {
+    res.json({ message: 'Hello' });
 })
 
-app.listen(7000,()=>{
+app.listen(7000, () => {
     console.log("Server started on localhost 7000")
 })
