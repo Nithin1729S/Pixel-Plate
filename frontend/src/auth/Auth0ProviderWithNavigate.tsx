4b9e6e8a0e45d4a3faa40ad4f6b2ce8a6dea61ed
nithin <sureshnithin1729@gmail.com>
2024-06-17 02:05:34 +0530

    security

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,41 +1,39 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 
 type Props = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const {createUser} =useCreateMyUser();
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const navigate=useNavigate();
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
-  if (!domain || !clientId || !redirectUri) {
-    throw new Error("unable to initialise auth");
-  }
+    if (!domain || !clientId || !redirectUri) {
+        throw new Error("unable to initialise auth");
+    }
 
-  const onRedirectCallback = (appState?: AppState,user?:User) => {
-        if(user?.sub && user?.email)
-            {
-                createUser({auth0Id:user.sub,email:user.email});
-            }
-  };
+    const onRedirectCallback = (appState?: AppState, user?: User) => {
+        console.log('User',user);
+        navigate('/auth-callback',)
+    };
 
-  return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: redirectUri
-        
-      }}
-      onRedirectCallback={onRedirectCallback}
-    >
-      {children}
-    </Auth0Provider>
-  );
+    return (
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{
+                redirect_uri: redirectUri
+
+            }}
+            onRedirectCallback={onRedirectCallback}
+        >
+            {children}
+        </Auth0Provider>
+    );
 };
 
 export default Auth0ProviderWithNavigate;
