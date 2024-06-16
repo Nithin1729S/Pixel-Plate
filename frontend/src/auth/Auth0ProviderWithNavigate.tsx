76f9a1f487dc7c723ec6c5dd31a0039cb78cc79c
nithin <sureshnithin1729@gmail.com>
2024-06-17 02:06:34 +0530

    done with 3rd session

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import {Auth0Provider} from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -16,8 +16,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         throw new Error("unable to initialise auth");
     }
 
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log('User',user);
+    const onRedirectCallback = () => {
         navigate('/auth-callback',)
     };
 
