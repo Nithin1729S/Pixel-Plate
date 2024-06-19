10c96e7695a91278f442c545b15823d420946d96
nithin <sureshnithin1729@gmail.com>
2024-06-19 22:01:30 +0530

    backend deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,9 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
 const app = express()
 app.use(express.json())
 app.use(cors())
-
+app.get("/health",async(req:Request,res:Response)=>{
+    res.send({message:"Health OK!"})
+});
 app.use("/api/my/user",myUserRoute);
 
 app.listen(7000, () => {
